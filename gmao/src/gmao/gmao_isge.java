43ebaf418b796f419cb100e9c458250636cc1b42
User <User@DESKTOP-5371ILI>
2024-06-17 17:20:33 +0000

    jjj

---
+++ gmao/src/gmao/gmao_isge.java
@@ -0,0 +1,43 @@
+package gmao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class gmao_isge {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					gmao_isge window = new gmao_isge();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public gmao_isge() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ gmao/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module gmao {
+	requires java.desktop;
+}
